b7415ab9de43cb5b099701875aad5792583d78be
wotosts <tosts522@gmail.com>
2022-10-15 00:07:05 +0900

    환경 설정

---
+++ week1/amber/android/app/src/main/kotlin/com/wotosts/amber/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wotosts.amber
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ week1/amber/lib/HomePage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Content(),
+    );
+  }
+}
+
+class Content extends StatefulWidget {
+  const Content({Key? key}) : super(key: key);
+
+  @override
+  State<Content> createState() => _ContentState();
+}
+
+class _ContentState extends State<Content> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.white);
+  }
+}
+
