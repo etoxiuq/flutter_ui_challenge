4492197dacb119cb9aec7ae9d4505739937a0f00
wotosts <tosts522@gmail.com>
2022-10-24 23:21:25 +0900

    week2 initail commit

---
+++ week2/amber/lib/ListScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:week2/components/AppBar.dart';
+import 'package:week2/components/SearchBar.dart';
+
+class ListScreen extends StatefulWidget {
+  const ListScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ListScreen> createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
