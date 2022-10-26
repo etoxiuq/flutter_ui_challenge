4b5270657d93a59083eafd9e6fe53ea45a093795
wotosts <tosts522@gmail.com>
2022-10-26 22:31:34 +0900

    week2: feat list 개발

---
+++ week2/amber/lib/model/School.dart
@@ -0,0 +1,16 @@
+class School {
+  School(this.name, this.location, this.category);
+
+  final String name;
+  final String location;
+  final String category;
+}
+
+var schoolList = [
+  School("Edgewick School", "579 Statan NY 12483", "Higher Secondary School"),
+  School("Xaviers International", "234 Road Kathmandu, Nepal", "Higher Secondary School"),
+  School("Kinder Garden", "579 Statan NY 12483", "Play Group School"),
+  School("WilingTon Cambridge", "Kasai Pantan NY 12483", "Lower Secondary School"),
+  School("Edgewick School", "Kasai Pantan 12483", "Higher Secondary School"),
+  School("Edgewick School", "579 Statan NY 12483", "Higher Secondary School"),
+];
